086b3f0639be57212426c376458d9ce99a9e0f08
Lichura <lichun88@gmail.com>
2016-09-14 11:22:24 +0200

    chau slack

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -1,4 +1,4 @@
 require 'slack-notifier'
 
 
-SLACK = Slack::Notifier.new "https://hooks.slack.com/services/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"
+SLACK = Slack::Notifier.new "https://hooks.slack.com/services/T2B58SZMM/B2BCHQS"
