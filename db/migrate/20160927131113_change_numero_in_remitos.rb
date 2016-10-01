1874304571a024c2e1984f38ef1e4fea7d8f4ec5
Lisandro Gurman <lichun88@gmail.com>
2016-10-01 22:57:11 +0200

    se corrigio una migracion para heroku

--- db/migrate/20160927131113_change_numero_in_remitos.rb
+++ db/migrate/20160927131113_change_numero_in_remitos.rb
@@ -1,6 +1,10 @@
 class ChangeNumeroInRemitos < ActiveRecord::Migration[5.0]
   def up
-    change_column :remitos, :numero, :integer
+   connection.execute(%q{
+    alter table remitos
+    alter column numero
+    type integer using cast(numero as integer)
+  })
   end
 
   def down
