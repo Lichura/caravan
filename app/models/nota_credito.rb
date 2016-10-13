40a307f793109cbe8dea4e16d3255fd8255afda7
Lisandro Gurman <lichun88@gmail.com>
2016-10-13 23:11:14 +0200

    arreglos en la homepage

--- app/models/nota_credito.rb
+++ app/models/nota_credito.rb
@@ -1,9 +1,13 @@
 class NotaCredito < ApplicationRecord
+		enum estados = {confirmado: 1,
+					    anulado: 2}
+
 	has_many :productos, through: :nota_credito_items
 	has_many :nota_credito_items
 
 	accepts_nested_attributes_for :nota_credito_items,  allow_destroy: true
 	before_validation :marcar_productos_para_destruir
+	after_create :estado_inicial
 
 	def marcar_productos_para_destruir
 	    nota_credito_items.each do |producto|
@@ -13,5 +17,7 @@ class NotaCredito < ApplicationRecord
     	end
   	end
 
-
+  	def estado_inicial
+  		self.confirmado!
+  	end
 end
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,1 +1,2 @@
 Time::DATE_FORMATS[:day_month_and_year] = "%d-%m-%Y"
+Time::DATE_FORMATS[:year] = "%Y"
