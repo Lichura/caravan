64734030381ff6b710f548e8378c6c02ed0184f5
Lichura <lgurman@MACAGS42.local>
2016-11-22 08:52:03 +0100

    correccion de rendimiento

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( scrollify.js )
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
 Rails.application.config.assets.precompile += %w( login.css )
 Rails.application.config.assets.precompile += %w( navbar.scss )
 Rails.application.config.assets.precompile += %w( index.scss )
