bd9d2b8645d786729ecbf499ed36feba0f9a8ff7
Lisandro Gurman <lichun88@gmail.com>
2017-01-02 21:25:29 +0100

    changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,17 +52,19 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  #config.action_mailer.default_url_options = {:host => "localhost:3000"}
+  config.action_mailer.default_url_options = {:host => "caravanastrenquelauquen.com.ar"}
+
+  ActionMailer::Base.raise_delivery_errors = true
 
       config.action_mailer.delivery_method = :smtp
 # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
    :address              => "smtp.migadu.com",
+   :domain               => "caravanastrenquelauquen.com.ar",
    :port                 => 587,
    :user_name            => "test@caravanastrenquelauquen.com.ar",
    :password             => "test1234",
-   :authentication       => :login,
-   :enable_starttls_auto => true
+   :authentication       => :plain
     }
 
   config.serve_static_assets = false
