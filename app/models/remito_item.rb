200b1350cd5653a7a1d79b2e1095c20f4ec29c9a
Lichura <lgurman@MACAGS42.local>
2017-06-07 15:54:32 +0200

    correcciones varias

--- app/models/remito_item.rb
+++ app/models/remito_item.rb
@@ -6,23 +6,29 @@ class RemitoItem < ApplicationRecord
 	after_destroy :aumentar_stock_al_eliminar_remito
 
 	def reducir_stock_al_remitir
-		self.remito.pedido.detalles.each do |detalle|
-			if detalle.detalle_insumos.any?
-				detalle.detalle_insumos.each do |detalle_insumo|
-					if detalle_insumo.producto_id == self.producto_id
-						insumo = Insumo.find(detalle_insumo.insumo_id)
-						coeficiente = ProductoInsumo.find_by(insumo_id: insumo.id, producto_id: self.producto_id).coeficiente
-						insumo.stock_reservado -= self.cantidad * coeficiente
-						insumo.stock_fisico -= self.cantidad * coeficiente
-						insumo.save
+		if !self.remito.pedido.nil?
+			self.remito.pedido.detalles.each do |detalle|
+				if detalle.detalle_insumos.any?
+					detalle.detalle_insumos.each do |detalle_insumo|
+						if detalle_insumo.producto_id == self.producto_id
+							insumo = Insumo.find(detalle_insumo.insumo_id)
+							coeficiente = ProductoInsumo.find_by(insumo_id: insumo.id, producto_id: self.producto_id).coeficiente
+							insumo.stock_reservado -= self.cantidad * coeficiente
+							insumo.stock_fisico -= self.cantidad * coeficiente
+							insumo.save
+						end
 					end
+				else
+					producto = Producto.find(self.producto_id)
+					producto.stock_reservado -= self.cantidad
+					producto.stock_fisico -= self.cantidad
+					producto.save
 				end
-			else
-				producto = Producto.find(self.producto_id)
-				producto.stock_reservado -= self.cantidad
-				producto.stock_fisico -= self.cantidad
-				producto.save
 			end
+		else
+			producto = Producto.find(self.producto_id)
+			producto.stock_fisico -= self.cantidad
+			producto.save
 		end
 	end
 
